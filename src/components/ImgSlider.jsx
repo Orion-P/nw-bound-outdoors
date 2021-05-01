d51da7fadff923c7f99cb49efd250746d3d6aa9c
Orion-P <orionpoptean@gmail.com>
2021-05-01 16:58:56 -0700

    carousel fix

--- src/components/ImgSlider.jsx
+++ src/components/ImgSlider.jsx
@@ -1,23 +1,21 @@
 import React from "react"
+import Loadable from "@loadable/component"
 
 import Carousel from "@brainhubeu/react-carousel"
 
 import "@brainhubeu/react-carousel/lib/style.css"
 import "../styles/ImgSlider.css"
 
-class MyCarousel extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <Carousel plugins={["arrows", "infinite"]}>
-        <img alt={this.props.alt} src={this.props.img1} />
-        <img alt={this.props.alt} src={this.props.img2} />
-        <img alt={this.props.alt} src={this.props.img3} />
-      </Carousel>
-    )
-  }
+const ImgSlider = props => {
+  return (
+    <Carousel plugins={["arrows", "infinite"]}>
+      <img alt={props.alt} src={props.img1} />
+      <img alt={props.alt} src={props.img2} />
+      <img alt={props.alt} src={props.img3} />
+    </Carousel>
+  )
 }
-export default MyCarousel
+
+const Caro = Loadable(() => import("./ImgSlider"))
+
+export default Caro
